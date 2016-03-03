514aa36883d27df3d02c58dd724e8fdfbff2a99f
nick soggin <nicksoggin@gmail.com>
2016-03-02 20:54:21 -0500

    First Commit

---
+++ playperl.sh
@@ -0,0 +1,3 @@
+perl -e 'for( $i = 0;; $i++ ) {
+			print pack( "n", ( ( ( $i * ( $i >> 8 | $i >> 9 ) &46& $i >> 8 ) ) ^ ( $i & $i >> 13 | $i >> 6 ) ) );
+		}' | ./play -c 2 -b 8 -e unsigned -t raw -r 48k -
